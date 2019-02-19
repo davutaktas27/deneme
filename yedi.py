bbe8cdd15f03feb883c11ae05769fa36be84b929
DavutAktas <davutaktas1991@gmail.com>
2019-02-19 13:57:29 +0300

    ilk commit

---
+++ yedi.py
@@ -0,0 +1,19 @@
+defkullanıcı="davut1"
+defsifre="123456"
+
+while True:
+    kullanıcı=input("kullanıcı adı: ")
+    sifre=input("sifre girin")
+    if(defkullanıcı!=kullanıcı or defsifre!=sifre):
+        print("Bilgiler yanlış..")
+    else:
+        print("Hoşgeldiniz...")
+        break
+
+listeler=[3,4,5]
+
+for i in range(1,10):
+    if(i in listeler):
+        continue
+    print(i)
+    #continue sonsuz döngü hatası verebilir.
